19827b8185e59a1dea20a3de8e9fb1ea6532c5b4
teanmichel <tean.koya@gmail.com>
2023-08-08 21:17:01 +0000

    screen waiting

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,148 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <!-- <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header> -->
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> Essential Links </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+    <q-footer class="bg-transparent q-pa-md bg-grey-4">
+      <div class="row justify-around">
+        <div class="row justify-around col-5">
+          <q-skeleton
+            size="1.8rem"
+            class="bg-white"
+            style="border-radius: 0.5rem"
+          />
+          <q-skeleton
+            size="1.8rem"
+            class="bg-white"
+            style="border-radius: 0.5rem"
+          />
+        </div>
+        <div class="col-2">
+          <div
+            class="row bg-white q-pa-sm absolute"
+            style="
+              top: 0;
+              right: 40vw;
+              transform: translateY(-60%);
+              border-radius: 50%;
+            "
+          >
+            <q-skeleton size="4rem" type="QAvatar" />
+          </div>
+        </div>
+        <div class="row justify-around col-5">
+          <q-skeleton
+            size="1.8rem"
+            class="bg-white"
+            style="border-radius: 0.5rem"
+          />
+          <q-skeleton
+            size="1.8rem"
+            class="bg-white"
+            style="border-radius: 0.5rem"
+          />
+        </div>
+      </div>
+    </q-footer>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
+
+const linksList = [
+  {
+    title: "Docs",
+    caption: "quasar.dev",
+    icon: "school",
+    link: "https://quasar.dev",
+  },
+  {
+    title: "Github",
+    caption: "github.com/quasarframework",
+    icon: "code",
+    link: "https://github.com/quasarframework",
+  },
+  {
+    title: "Discord Chat Channel",
+    caption: "chat.quasar.dev",
+    icon: "chat",
+    link: "https://chat.quasar.dev",
+  },
+  {
+    title: "Forum",
+    caption: "forum.quasar.dev",
+    icon: "record_voice_over",
+    link: "https://forum.quasar.dev",
+  },
+  {
+    title: "Twitter",
+    caption: "@quasarframework",
+    icon: "rss_feed",
+    link: "https://twitter.quasar.dev",
+  },
+  {
+    title: "Facebook",
+    caption: "@QuasarFramework",
+    icon: "public",
+    link: "https://facebook.quasar.dev",
+  },
+  {
+    title: "Quasar Awesome",
+    caption: "Community Quasar projects",
+    icon: "favorite",
+    link: "https://awesome.quasar.dev",
+  },
+];
+
+export default defineComponent({
+  name: "MainLayout",
+
+  components: {
+    EssentialLink,
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,148 @@
+<template>
+  <q-page padding class="">
+    <div class="row q-pa-md q-mt-md">
+      <q-skeleton size="2.8rem" style="border-radius: 0.5rem" />
+      <q-space />
+      <q-skeleton size="2.8rem" style="border-radius: 0.5rem" />
+    </div>
+    <section class="q-pa-md">
+      <!-- CArte d'article en vedette -->
+      <q-skeleton
+        height="10rem"
+        square
+        class="q-pa-md row items-end bg-grey-7"
+        style="border-radius: 0.8rem"
+      >
+        <q-skeleton
+          square
+          height="1.8rem"
+          width="8rem"
+          style="border-radius: 0.5rem"
+          class="bg-white"
+        />
+      </q-skeleton>
+    </section>
+    <div class="q-px-md">
+      <q-skeleton type="text" width="40vw" style="border-radius: 0.5rem" />
+    </div>
+    <section class="q-py-md">
+      <q-tabs
+        v-model="tab"
+        dense
+        inline-label
+        indicator-color="transparent"
+        class="bg-transparent"
+      >
+        <q-tab name="mails" :ripple="false" class="">
+          <q-skeleton
+            square
+            height="2.3rem"
+            width="8rem"
+            class="bg-grey-4"
+            style="border-radius: 0.5rem"
+          />
+        </q-tab>
+        <q-tab name="alarms" class="q-px-sm" :ripple="false">
+          <q-skeleton
+            square
+            height="2.3rem"
+            width="6rem"
+            class="bg-grey-4"
+            style="border-radius: 0.5rem"
+          />
+        </q-tab>
+        <q-tab name="movies" :ripple="false">
+          <q-skeleton
+            square
+            height="2.3rem"
+            width="8rem"
+            class="bg-grey-4"
+            style="border-radius: 0.5rem"
+          />
+        </q-tab>
+        <q-tab name="photos" class="q-px-sm" :ripple="false">
+          <q-skeleton
+            square
+            height="2.3rem"
+            width="5rem"
+            class="bg-grey-4"
+            style="border-radius: 0.5rem"
+          />
+        </q-tab>
+        <q-tab name="videos" :ripple="false">
+          <q-skeleton
+            square
+            height="2.3rem"
+            width="7rem"
+            class="bg-grey-4"
+            style="border-radius: 0.5rem"
+          />
+        </q-tab>
+        <q-tab name="addressbook" class="q-px-sm" :ripple="false">
+          <q-skeleton
+            square
+            height="2.3rem"
+            width="8rem"
+            class="bg-grey-4"
+            style="border-radius: 0.5rem"
+          />
+        </q-tab>
+      </q-tabs>
+      <q-tab-panels v-model="tab" animated dense class="q-px-md">
+        <q-tab-panel name="mails" class="q-px-none q-gutter-y-sm">
+          <q-skeleton
+            v-for="n in 2"
+            :key="n"
+            height="9rem"
+            square
+            class="q-pa-sm row items-center bg-grey-4"
+            style="border-radius: 0.8rem"
+          >
+            <q-skeleton
+              square
+              height="7rem"
+              width="7rem"
+              style="border-radius: 0.5rem"
+              class="bg-grey-7"
+            />
+          </q-skeleton>
+        </q-tab-panel>
+
+        <q-tab-panel name="alarms">
+          <q-skeleton
+            height="9rem"
+            square
+            class="q-pa-sm row items-center bg-grey-4"
+            style="border-radius: 0.8rem"
+          >
+            <q-skeleton
+              square
+              height="7rem"
+              width="7rem"
+              style="border-radius: 0.5rem"
+              class="bg-grey-7"
+            />
+          </q-skeleton>
+        </q-tab-panel>
+
+        <q-tab-panel name="movies">
+          <div class="text-h6">Movies</div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </q-tab-panel>
+      </q-tab-panels>
+    </section>
+  </q-page>
+</template>
+
+<script>
+import { ref, defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+  setup() {
+    return {
+      tab: ref("mails"),
+    };
+  },
+});
+</script>
